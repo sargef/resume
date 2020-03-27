3d154a7523a60080289caa3f516c5a8f85f44e1b
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-03-27 14:44:27 +1030

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,20 @@ router.get('/about', (req, res, next) => {
     res.render('about');
  });
 
+// Project pages route & if statement to catch 404 error
+router.get('/projects/:id', (req, res, next) => {
+    const { id } = req.params;
+    const projectTemplate = { 
+        project: data.projects[id]
+    }
+    if (projectTemplate.project) {
+        res.render('project', projectTemplate);
+    } else {
+        const error = new Error('The page you were looking for does not exist');
+        error.status = 404;
+        console.log(error.message, error.status)
+        next(error);
+    }   
+})
+
 module.exports = router;
