2950c096752188510214c29c595823dc6f6eb0f1
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-03-29 11:43:09 +1030

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Required packages for project
 const express = require('express');
+const httpStatus = require('http-status-codes');
 const bodyParser = require('body-parser');
 const data = require('./data.json');
 const pug = require('pug');
@@ -23,9 +24,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Imports page route objects
 app.use('/', indexRouter);
 
+// catch 404 Error
+app.use((req, res, next) => {
+  const err = new Error('There is no such page, sorry!');
+  err.status = 404;
+  next(err);
+});
+
+// Development Error Handler
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+    console.log('There is no such page, sorry!', err.status);
+  });
+}
+
+//Production Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+  console.log('There is no such page, sorry!', err.status);
+});
+
 module.exports = app;
 
- // Setting port to listen on port 8080
+ // Setting port to listen on port 3000
  app.listen(port, function() {
     console.log('Your app is listening on port:' + port);
 });
